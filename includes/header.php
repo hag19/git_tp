5ae2a74be7dc63ce8ba6c2238f492c21792eef4b
Malo Laval <laboitedemalo@gmail.com>
2024-11-08 20:26:27 +0100

    malo

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
     <nav>
         <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) : ?>
             <a href="profil.php">Profil</a>
-            <a href="mes-events.php">Mes événements</a>
+            <a href="my-events.php">Mes événements</a>
             <a href="event-list.php">Lister événements</a>
             <a href="creation-event.php">Créer événement</a>
             <a href="logout.php">Déconnexion</a>
